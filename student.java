f890e6a6ed38672042f8aa88cbf0c4762686f0a1
NidhiNarayan22 <nidhi.narayan@acuverconsulting.com>
2025-08-26 14:39:54 +0530

    26_08_2025-2:39

---
+++ student.java
@@ -0,0 +1,20 @@
+public class student {
+    String name;
+    int age;
+
+    // Constructor
+    public student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        student s1 = new student("Nidhi", 22);
+        s1.displayInfo();
+    }
+}
